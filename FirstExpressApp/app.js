0433046c8cab64abf3101eafe9f38d187832943f
User <user@Mac.local>
2018-05-03 10:41:39 +1000

    added routes

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -15,9 +15,22 @@ app.get('/dog', function(req, res) {
   res.send('Miaw');
 });
 
-app.get('*', function(req, res) {
-  res.send('You are a star');
+app.get('/r/:subredditName', function(req, res) {
+  var subreddit = req.params.subredditName;
+  res.send('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT');
 });
+
+
+app.get('/r/:subredditName/comment/:id/:title', function(req, res) {
+  console.log(req.params);
+  res.send('Welcome to comments');
+});
+
+app.get('*', function(req, res) {
+
+  res.send('You are a star!');
+});
+
 
 
 app.listen(3000, process.env.IP, function() {
